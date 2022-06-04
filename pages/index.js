43040e198bd48059876e9e9b1771d2898cae93c7
njina <najina@gmail.com>
2022-06-04 12:53:26 +0900

    all files

---
+++ pages/index.js
@@ -0,0 +1,7 @@
+import Head from "next/head";
+import Link from "next/link";
+import styles from "../styles/Home.module.css";
+
+export default function Home() {
+  return <div>landing page - one page</div>;
+}
