525dae98aa43f7e5e8012ffe51015dff3a18a8c4
njina <najina@gmail.com>
2022-06-04 12:58:16 +0900

    modify footer

--- pages/index.js
+++ pages/index.js
@@ -3,5 +3,14 @@ import Link from "next/link";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
-  return <div>landing page - one page</div>;
+  return (
+    <>
+      <Head>
+        <title>landing page</title>
+      </Head>
+      <footer>
+        <p>서울특별시 금천구 가산디지털1로 skv1 1606호</p>
+      </footer>
+    </>
+  );
 }
