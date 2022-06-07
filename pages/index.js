cfca2645da2d716703847b13f3c2d879827125fd
njina <najina@gmail.com>
2022-06-07 11:26:36 +0900

    re-deploy

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,10 @@
 import Head from "next/head";
-import Link from "next/link";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 import Posts from "../components/Posts";
 import Contact from "../components/Contact";
-import baseApiUrl from "../utils/baseApiUrl";
 
-export default function Home({ galleryData }) {
+export default function Home({}) {
   return (
     <>
       <Head>
@@ -14,21 +12,9 @@ export default function Home({ galleryData }) {
       </Head>
 
       <Header />
-      <Posts galleryData={galleryData} />
+      <Posts />
       <Contact />
       <Footer />
     </>
   );
-}
-
-export async function getServerSideProps(context) {
-  let { id } = context.query;
-  const galleryRes = await fetch(`${baseApiUrl}/api/galleries?populate=*`);
-  const galleryData = await galleryRes.json();
-
-  return {
-    props: {
-      galleryData,
-    }, // will be passed to the page component as props
-  };
 }
